11f288468029991cd7b82669a7cb768dca61e9a6
Mert <102308809+Awaria1337@users.noreply.github.com>
2025-02-10 10:47:12 +0300

    Update page.js

--- src/app/admin/questions/page.js
+++ src/app/admin/questions/page.js
@@ -1,4 +1,5 @@
 'use client';
+export const dynamic = 'force-dynamic';
 import { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import { FiPlus, FiEdit2, FiTrash2, FiDownload, FiUpload, FiPieChart } from 'react-icons/fi';
